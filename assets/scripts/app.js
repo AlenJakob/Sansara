38c191672c507bf0f15dc63c7fb7ac046cdcd648
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-04-07 06:29:44 +0200

    animation ver1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,58 @@
-document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-  console.log("IS RUNNING !");
-  document.querySelectorAll(".header__image").forEach((e) => console.log(e));
-}
+
+const header = document.querySelector(".body__header");
+const sectionOne = document.querySelector(".body__hero");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new interActionSectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+      header.classList.remove("nav-scrolled-out");
+
+    } else {
+      header.classList.remove("nav-scrolled");
+
+      header.classList.add("nav-scrolled-out");
+    }
+  });
+},
+  sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px"
+};
+
+const appearOnScroll = new interActionSectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+  appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
