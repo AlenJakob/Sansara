f21ed35a64de85bd453174050f485aae46f8255b
alen <grafuxa@gmail.com>
2021-04-07 14:19:58 +0200

    updated responsive ver2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,44 +1,40 @@
-
-
 const header = document.querySelector(".body__header");
 const sectionOne = document.querySelector(".body__hero");
 
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 const sectionOneOptions = {
-  rootMargin: "-200px 0px 0px 0px"
+  rootMargin: "-200px 0px 0px 0px",
 };
 
-const sectionOneObserver = new interActionSectionObserver(function (
+const sectionOneObserver = new IntersectionObserver(function (
   entries,
   sectionOneObserver
 ) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       header.classList.add("nav-scrolled");
       header.classList.remove("nav-scrolled-out");
-
     } else {
       header.classList.remove("nav-scrolled");
-
       header.classList.add("nav-scrolled-out");
     }
   });
 },
-  sectionOneOptions);
+sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
 
 const appearOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -200px 0px"
+  rootMargin: "0px 0px -200px 0px",
 };
 
-const appearOnScroll = new interActionSectionObserver(function (
+const appearOnScroll = new IntersectionObserver(function (
   entries,
   appearOnScroll
 ) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
@@ -47,12 +43,12 @@ const appearOnScroll = new interActionSectionObserver(function (
     }
   });
 },
-  appearOptions);
+appearOptions);
 
-faders.forEach(fader => {
+faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
+sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
