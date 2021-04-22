9a2e0033bd4e4adea3787c5813be66878efff24c
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-04-22 06:53:20 +0200

    Ie issue animation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,4 @@
 
-
-
 const header = document.querySelector(".section__header");
 const sectionOne = document.querySelector(".section__hero");
 const faders = document.querySelectorAll(".fade-in");
@@ -9,56 +7,82 @@ const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px",
 };
 
-const sectionOneObserver = new IntersectionObserver(function (
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      header.classList.add("nav-scrolled");
-      header.classList.remove("nav-scrolled-out");
-    } else {
-      header.classList.remove("nav-scrolled");
-      header.classList.add("nav-scrolled-out");
-    }
+function runAnimation() {
+
+  const sectionOneObserver = new IntersectionObserver(function (
+    entries,
+    sectionOneObserver
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("nav-scrolled");
+        header.classList.remove("nav-scrolled-out");
+      } else {
+        header.classList.remove("nav-scrolled");
+        header.classList.add("nav-scrolled-out");
+      }
+    });
+  },
+    sectionOneOptions);
+
+  sectionOneObserver.observe(sectionOne);
+
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px",
+  };
+
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+      }
+    });
+  },
+    appearOptions);
+
+  faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
   });
-},
-  sectionOneOptions);
 
-sectionOneObserver.observe(sectionOne);
-
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -200px 0px",
-};
-
-const appearOnScroll = new IntersectionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-    }
+  sliders.forEach((slider) => {
+    appearOnScroll.observe(slider);
   });
-},
-  appearOptions); 
 
-faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
-});
+  // DOM ELEMENTS
+  const scrollByButton = document.getElementById("scrollBy");
+  const headerUnpack = document.querySelector(".section-summary")
+  // SCROLL TO
+  scrollByButton.addEventListener("mouseenter", function () {
+    headerUnpack.scrollIntoView(true);
+  });
 
-sliders.forEach((slider) => {
-  appearOnScroll.observe(slider);
-});
+}
+function isIE() {
+  const ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+  const msie = ua.indexOf('MSIE '); // IE 10 or older
+  const trident = ua.indexOf('Trident/'); //IE 11
 
-// DOM ELEMENTS
-const scrollByButton = document.getElementById("scrollBy");
-const headerUnpack = document.querySelector(".section-summary")
-// SCROLL TO
-scrollByButton.addEventListener("mouseenter", function () {
- headerUnpack.scrollIntoView(true);
-});
+  return (msie > 0 || trident > 0);
+}
 
+function init() {
+  console.log(isIE())
+  if (isIE()) {
+    // entry.target.classList.add("appear");
+    console.log("is IE");
+    return;
+  } 
+
+    runAnimation()
+
+
+}
+
+
+init();
